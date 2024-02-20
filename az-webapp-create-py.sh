2d762f6541a7b7e53179cc38a6719165565c7744
vinothrao <vinothrao89@gmail.com>
2024-02-20 11:59:08 +0530

    Update az-webapp-create-py.sh

--- az-webapp-create-py.sh
+++ az-webapp-create-py.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -f deploy.zip
-zip -r deploy.zip ./app/backend -x ./.git\* ./.vscode\* ./deploy\*  
+rm -f ./deploy.zip
+zip -r ./deploy.zip ./app/backend -x ./.git\* ./.vscode\* ./deploy\*  
 
 az webapp config appsettings set --resource-group jenkins-test --name jenkins-py-test --settings SCM_DO_BUILD_DURING_DEPLOYMENT=true 
 if [ ! -z "$BUILDTAG" ]; then
     az webapp config appsettings set -g "jenkins-test" -n "jenkins-py-test" 
 fi
-az webapp deploy -g "jenkins-test" -n "jenkins-py-test" --src-path deploy.zip --type "zip"
+az webapp deploy -g "jenkins-test" -n "jenkins-py-test" --src-path ./deploy.zip --type "zip"
